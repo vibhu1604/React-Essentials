cfcd5288ced270bc0694e13a5257e2d69a7eb021
Vibhuti Gohil <31760633+vibhu1604@users.noreply.github.com>
2024-07-09 19:47:45 -0400

    Update CoreConcepts.jsx

--- src/components/CoreConcepts.jsx
+++ src/components/CoreConcepts.jsx
@@ -1,16 +1,14 @@
 import { CORE_CONCEPTS } from "../data.js";
 import CoreConcept from "./CoreConcept.jsx";
-
+import Section from "./Section.jsx";
 export default function CoreConcepts() {
   return (
-    <section id="core-concepts">
-      <h2>
+    <Section id="core-concepts" title="Core Concepts">
         <ul>
           {CORE_CONCEPTS.map((conceptItems) => (
             <CoreConcept key={conceptItems.title} {...conceptItems} />
           ))}
         </ul>
-      </h2>
-    </section>
+    </Section>
   );
 }
