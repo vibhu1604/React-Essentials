b9e9775212989c8faa5a6c3a65b971f65256583d
Vibhuti Gohil <31760633+vibhu1604@users.noreply.github.com>
2024-07-09 19:47:56 -0400

    Create Tabs.jsx

---
+++ src/components/Tabs.jsx
@@ -0,0 +1,11 @@
+import { children } from "react"
+
+export default function Tabs({children, buttons, ButtonsContainer = 'menu'}){
+    //const ButtonsContainer = buttonsContainer;
+    return(
+        <>
+            <ButtonsContainer>{buttons}</ButtonsContainer>
+            {children}
+        </>
+    );
+}
